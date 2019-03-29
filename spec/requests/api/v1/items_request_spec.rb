5a5b2cef0320a4b21756fd2437e75cd6d0ee212b
Sabrina Robinson <sabrinarob@gmail.com>
2019-03-29 10:32:13 -0600

    TEST - Item show

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,4 +1,4 @@
-require "rails_helper"
+require 'rails_helper'
 
 describe 'Items API' do
   it 'Sends all items' do
@@ -7,5 +7,19 @@ describe 'Items API' do
     get '/api/v1/items'
 
     expect(response).to be_successful
+
+    items = JSON.parse(response.body)
+    expect(items.count).to eq(3)
+  end
+
+  it 'Sends item by id' do
+    id =  create(:item).id
+
+    get "api/v1/items/#{id}"
+
+    item =  JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(item["id"]).to eq(id)
   end
 end
