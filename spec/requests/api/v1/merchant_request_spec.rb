dc89f22cb0bfafc62005531571dee67c2ee9e355
Sabrina Robinson <sabrinarob@gmail.com>
2019-03-30 15:59:51 -0600

    TEST - Merchant index

---
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Merchants API' do
+  it 'sends all merchants' do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response.status).to eq(200)
+
+    merchants = JSON.parse(response.body, symbolize_names: ture)
+
+    expect(merchants.count).to eq(3)
+    expect(merchants[0]).to have_key(:id)
+    expect(merchants[0]).to have_key(:name)
+    expect(merchants[0]).to have_key(:created_at)
+    expect(merchants[0]).to have_key(:updated_at)
+  end
+end
