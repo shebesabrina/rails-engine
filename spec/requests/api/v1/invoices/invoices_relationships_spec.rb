a549c8d3b7b0f0b75c8985663345ce85ab1e30fa
Sabrina Robinson <sabrinarob@gmail.com>
2019-04-01 11:00:08 -0600

    TEST - returns a collection of associated invoice items

--- spec/requests/api/v1/invoices/invoices_relationships_spec.rb
+++ spec/requests/api/v1/invoices/invoices_relationships_spec.rb
@@ -16,4 +16,19 @@ describe 'Invoices API relationships' do
     expect(transactions).to eq(invoice.transactions)
   end
 
+  it 'returns a collection of associated invoice items' do
+    invoice =  create(:invoice)
+
+    get "/api/v1/invoices/#{invoice.id}/invoice_items"
+
+    invoice_items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.body).to be_successful
+    invoice_items.each do |ii|
+      expect(ii['invoice_id']).to eq(invoice.id)
+    end
+
+    expect(invoice_items).to eq(invoice_id.invoice_items)
+  end
+
 end
