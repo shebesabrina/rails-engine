c83cb3f3113915eb48233e546279a88de5e9a3f7
Sabrina Robinson <sabrinarob@gmail.com>
2019-04-02 11:16:14 -0600

    TEST - returns a collection of associated transactionss

--- spec/requests/api/v1/customers/relationship_spec.rb
+++ spec/requests/api/v1/customers/relationship_spec.rb
@@ -8,10 +8,25 @@ describe 'Customer API relationships' do
 
     expect(response).to be_successful
 
-    invoices = JSON.parse(response.body)
+    invoices = JSON.parse(response.body, symbolize_names: true)
     invoices.each do |invoice|
-      expect(invoice['customer_id']).to eq(customer.id)
+      expect(invoice[:customer_id]).to eq(customer.id)
     end
     expect(invoices).to eq(customer.invoices)
+  end
+
+  it 'returns a collection of associated transactions' do
+    customer = create(:customer)
+
+    get "/api/v1/customers/#{customer.id}/transactions"
+    expect(response).to be_success
+
+    transactions = JSON.parse(response.body, symbolize_names: true)
+
+    transactions.each do |transaction|
+      expect(transaction[:customer_id]).to eq(customer.id)
+    end
+
+    expect(transactions).to eq(customer.transactions)
   end
 end
